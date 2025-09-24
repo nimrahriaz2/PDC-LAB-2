6906ec1e90e421c684a9344a420a78b0ee24b283
nimrahriaz2 <nimrahriaz2@gmail.com>
2025-09-24 08:47:06 +0500

    Add files via upload

---
+++ Task 1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Please enter your name: ");
+        string username = Console.ReadLine();
+
+
+        for (int i = 5; i >= 0; i--)
+        {
+            Console.WriteLine(i);
+            if (i == 5)
+                Thread.Sleep(500);
+            else
+                Thread.Sleep(1000);
+        }
+
+        Console.WriteLine($"Welcome {username}!");
+    }
+}
