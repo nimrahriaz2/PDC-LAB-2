cda90166bef0a827a211329cfd63c0fd9badd8b8
nimrahriaz2 <nimrahriaz2@gmail.com>
2025-09-24 10:18:57 +0500

    Add files via upload

---
+++ Task 3.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading;
+class RainSimulation { static void Main() {
+        string[] frames = new string[] {
+@"
+.     .   .    '    .     .   .     .
+   '     .   .    .     '      .     
+.   .    .   '     .   .    .     .
+     .    '   .      .     .   '   .
+",
+
+@"
+   .    '   .     .    .      '     
+.     .     .  '     .     .     .
+    '     .    .   .     '      .  
+.    .    .   '     .    .     .   
+",
+
+@"
+.  '   .     .    .     .   '    . 
+    .     .     '     .    .    .  
+.     '     .    .     .     '     
+   .    .     .   '    .      .   .
+"
+};
+        Console.CursorVisible = true; for (int i = 0; i < 50; i++)
+        { foreach (string frame in frames) { Console.SetCursorPosition(0, 0);
+                Console.Write(frame);
+                Thread.Sleep(50); } }
+        Console.CursorVisible = true;
+    }
+}
