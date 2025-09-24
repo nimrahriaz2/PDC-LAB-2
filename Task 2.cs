b09a67f1d360f644133b318eb96a55c80ef9c326
nimrahriaz2 <nimrahriaz2@gmail.com>
2025-09-24 09:18:02 +0500

    Add files via upload

---
+++ Task 2.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading;
+class Program
+{
+    static void Main()
+    {
+        string line = "----------------";
+        int length = line.Length;
+        for (int i = 0; i < length; i++) {
+            Console.WriteLine('\r' + new string(' ', i) + "C" + line.Substring(i + 1));
+            Thread.Sleep(200);
+            Console.WriteLine('\r' + new string(' ', i) + "C" + line.Substring(i + 1));
+            Thread.Sleep(200);
+
+
+        }
+        Console.WriteLine('\r' + new string(' ', length + 'C'));
+        Console.WriteLine("\n \nGame Over! Pac-Man ate all dots");
+
+
+    }
+}
